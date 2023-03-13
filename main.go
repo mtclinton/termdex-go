787974c523f7b5002477769fb8d57412da30a2eb
Max Clinton <maxtclinton@gmail.com>
2023-03-13 15:19:49 -0400

    Adding search functionality

--- main.go
+++ main.go
@@ -2,8 +2,57 @@ package main
 
 import "fmt"
 import "termdex-go/pokeball"
+import "os"
+import "bufio"
+import "strconv"
+import "log"
+import "strings"
+import "net/http"
+import "encoding/json"
+
+type PokeData struct{
+    Name              string `json:"name"`
+}
+
+func searchPokemon(pokemon_id int) {
+    url := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%d", pokemon_id)
+
+	// Build the request
+	req, err := http.NewRequest("GET", url, nil)
+
+    if err != nil {
+		log.Fatal("NewRequest: ", err)
+		return
+	}
+
+    client := &http.Client{}
+
+    resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		return
+	}
+
+    defer resp.Body.Close()
+
+    var pokemon PokeData
+
+    if err := json.NewDecoder(resp.Body).Decode(&pokemon); err != nil {
+		log.Println(err)
+	}
+    fmt.Println(pokemon.Name)
+}
 
 func main() {
-    pokeball.ShowPokeball()
-    fmt.Println("Hello, world.")
+	pokeball.ShowPokeball()
+	fmt.Println("Welcome to TermDex")
+	fmt.Println("Input a pokemon ID")
+	reader := bufio.NewReader(os.Stdin)
+	userInput, _ := reader.ReadString('\n')
+    userInput = strings.ReplaceAll(userInput, "\n", "")
+    pokemon_id, err := strconv.Atoi(userInput)
+    if err != nil {
+        log.Fatal(err)
+    }
+    searchPokemon(pokemon_id)
 }
