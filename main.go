e610ca8f274adac12f39d14d876fbd37087da387
Max Clinton <maxtclinton@gmail.com>
2023-03-13 20:02:27 -0400

    Display sprite in color

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import "strings"
 import "net/http"
 import "encoding/json"
 import "io/ioutil"
+import "github.com/gookit/color"
+import "errors"
+
 
 type TypeName struct {
     Name            string `json:"name"`
@@ -24,18 +27,57 @@ type PokeData struct{
     Types             []PokeType `json:"types"`
 }
 
-func show_pokemon(pokemon_id int) {
+
+
+
+func show_sprite(sprite string, poke_type string) {
+    poke_colors := map[string][]int{
+        "normal": []int{168, 167, 122},
+        "fire": []int{238, 129, 48},
+        "water": []int{99, 144, 240},
+        "electric": []int{247, 208, 44},
+        "grass": []int{122, 199, 76},
+        "ice": []int{150, 217, 214},
+        "fighting": []int{194, 46, 40},
+        "poison": []int{163, 62, 161},
+        "ground": []int{226, 191, 101},
+        "flying": []int{169, 143, 243},
+        "psychic": []int{249, 85, 135},
+        "bug": []int{166, 185, 26},
+        "rock": []int{182, 161, 54},
+        "ghost": []int{115, 87, 151},
+        "dragon": []int{111, 53, 252},
+        "dark": []int{112, 87, 70},
+        "steel": []int{183, 183, 206},
+        "fairy": []int{214, 133, 173},
+    }
+    val, ok := poke_colors[poke_type]
+    if ok {
+        r, g, b := val[0], val[1], val[2]
+        colorSprite := "<fg="+strconv.Itoa(r)+","+strconv.Itoa(g)+","+strconv.Itoa(b)+">"+sprite+"</>"
+        color.Println(colorSprite)
+    } else {
+        log.Fatal("Incorrect type submitted")
+    }
+}
+
+func show_pokemon(pokemon_id int) (string, error) {
     jsonPokemon, err := os.Open("pokemon.json")
     if err != nil {
         log.Fatal("NewRequest: ", err)
-         return
+         return "", err
      }
     defer jsonPokemon.Close()
     byteResult, _ := ioutil.ReadAll(jsonPokemon)
-
-   var res map[string]interface{}
-   json.Unmarshal([]byte(byteResult), &res)
-    fmt.Println(res[strconv.Itoa(pokemon_id)])
+    var res map[string]string
+    json.Unmarshal([]byte(byteResult), &res)
+    val, ok := res[strconv.Itoa(pokemon_id)]
+    if ok {
+        return val, nil
+    } else {
+        log.Fatal("Incorrect pokemon id submitted")
+        return "", errors.New("Incorrect pokemon id submitted")
+    }
 }
 
 func searchPokemon(pokemon_id int) {
@@ -68,16 +110,17 @@ func searchPokemon(pokemon_id int) {
     for _, ptype := range pokemon.Types {
         pokemon_types = append(pokemon_types, ptype.Type.Name)
     }
-    show_pokemon(pokemon_id)
+    sprite, _ := show_pokemon(pokemon_id)
+    show_sprite(sprite, pokemon_types[0])
     fmt.Println(pokemon.Name)
 }
 
 func main() {
-	pokeball.ShowPokeball()
-	fmt.Println("Welcome to TermDex")
-	fmt.Println("Input a pokemon ID")
-	reader := bufio.NewReader(os.Stdin)
-	userInput, _ := reader.ReadString('\n')
+   pokeball.ShowPokeball()
+    fmt.Println("Welcome to TermDex")
+    fmt.Println("Input a pokemon ID")
+    reader := bufio.NewReader(os.Stdin)
+    userInput, _ := reader.ReadString('\n')
     userInput = strings.ReplaceAll(userInput, "\n", "")
     pokemon_id, err := strconv.Atoi(userInput)
     if err != nil {
